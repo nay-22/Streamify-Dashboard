e5e0821f38be1b91938c396bed992c130ecc9b58
Nayan <nayan92001@gmail.com>
2025-02-03 06:46:24 +0530

    Carousel View of charts to support multiple screen sizes

--- Streamify-frontend/src/components/DataVisuals.tsx
+++ Streamify-frontend/src/components/DataVisuals.tsx
@@ -1,9 +1,58 @@
-import { useFetch } from "../hooks";
+import ChartCard from "./ChartCard";
+import RevenueDistributionChart from "./Charts/RevenueDistributionChart";
+import TopStreamChart from "./Charts/TopStreamChart";
+import UserGrowthChart from "./Charts/UserGrowthChart";
+import useStreamify from "../hooks/useStreamify";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
 
 const DataVisuals = () => {
+  const { windowSize } = useStreamify();
+  const { width } = windowSize;
+
+  const slidesPerView =
+    width > 1430 ? 3 : width > 1015 ? 2 : 1;
+
+  const generateContainerClassName = (width: number) => {
+    if (width < 400) {
+      return "px-4 pt-2 pb-2"
+    }
+    else if (width < 500) {
+      return "px-8 pt-4 pb-4";
+    } else {
+      return "px-12 pt-6 pb-6";
+    }
+  };
+
   return (
-    <div className="flex items-center justify-evenly px-8 py-4 gap-8">
-      DataVisuals
+    <div className={generateContainerClassName(width)}>
+      <Swiper
+        slidesPerView={slidesPerView}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+      >
+        <SwiperSlide>
+          <ChartCard title="User Growth">
+            <UserGrowthChart />
+          </ChartCard>
+        </SwiperSlide>
+        <SwiperSlide>
+          <ChartCard title="Revenue Distribution">
+            <RevenueDistributionChart />
+          </ChartCard>
+        </SwiperSlide>
+        <SwiperSlide>
+          <ChartCard title="Top 5 Streams">
+            <TopStreamChart />
+          </ChartCard>
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
