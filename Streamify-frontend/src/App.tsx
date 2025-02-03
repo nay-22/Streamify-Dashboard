e230e11448d5ff0ee67a935d0ba84f1696f1f399
Nayan <nayan92001@gmail.com>
2025-02-03 06:42:36 +0530

    Context Setup and added new tailwind theme colors

--- Streamify-frontend/src/App.tsx
+++ Streamify-frontend/src/App.tsx
@@ -3,17 +3,21 @@ import DataVisuals from "./components/DataVisuals";
 import Header from "./components/Header";
 import KeyMetrics from "./components/KeyMetrics";
 import RecentsTable from "./components/RecentsTable";
+import { useScreenSize } from "./hooks";
+import StreamifyProvider from "./context/providers/StreamifyProvider";
 
 function App() {
+  const { screen, windowSize } = useScreenSize();
+
   return (
-    <>
+    <StreamifyProvider value={{ screen, windowSize }}>
       <div className="bg-dark-bg-primary text-dark-text-primary min-h-screen">
         <Header />
         <KeyMetrics />
         <DataVisuals />
         <RecentsTable />
       </div>
-    </>
+    </StreamifyProvider>
   );
 }
 
