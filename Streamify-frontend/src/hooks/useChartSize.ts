e95d76bc5da2cb253c7a74dc8e52a73e313d20da
Nayan <nayan92001@gmail.com>
2025-02-04 00:40:17 +0530

    Reworked useFetch() to included query options generics

--- Streamify-frontend/src/hooks/useChartSize.ts
+++
@@ -1,8 +0,0 @@
-import useScreenSize from "./useScreenSize";
-
-const useChartSize = () => {
-  const { windowSize } = useScreenSize();
-  const { width } = windowSize;
-
-  
-};
--- Streamify-frontend/src/hooks/useFetch.ts
+++ Streamify-frontend/src/hooks/useFetch.ts
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 
 export type Options = {
   method?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE" | "HEAD";
@@ -7,6 +7,10 @@ export type Options = {
   };
   body?: string | null;
   credentials?: "include" | "omit" | "same-origin";
+};
+
+export type QueryOptions = {
+  [key: string]: string | number;
 };
 
 /**
@@ -19,35 +23,75 @@ export type Options = {
  * @param options Fetch request options object
  * @returns  Array [data, isLoading, error]
  */
-const useFetch = <T>(
+const useFetch = <T, S extends QueryOptions = QueryOptions>(
   url: string,
+  queryOptions?: S,
   options: Options = { method: "GET" }
-): { data: T | undefined; isLoading: boolean; error: string | null } => {
+): {
+  data: T | undefined;
+  query: S | undefined;
+  addQuery: (query: S) => void;
+  deleteQuery: (key: string) => void;
+  updateQuery: (key: string, value: string | number) => void;
+  isLoading: boolean;
+  error: string | null;
+} => {
   const [data, setData] = useState<T | undefined>(undefined);
+  const [query, setQuery] = useState<S | undefined>(queryOptions);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
-  const OPTIONS = useMemo(
-    () => ({
+  const optionsRef = useRef<Options>(options);
+
+  useEffect(() => {
+    optionsRef.current = {
       method: options.method || "GET",
       body: options.body ? JSON.stringify(options.body) : null,
       headers: {
-        "Content-type": "application/json",
+        "Content-Type": "application/json",
         ...options.headers,
       },
       credentials: options.credentials || "same-origin",
-    }),
-    [options.method, options.body, options.headers, options.credentials]
-  );
+    };
+  }, [options]);
+
+  const addQuery = (query: S) => {
+    setQuery((prev) => ({ ...(prev ?? {}), ...query } as S));
+  };
+
+  const updateQuery = (key: string, value: string | number) => {
+    setQuery((prev) => ({ ...(prev ?? {}), [key]: value } as S));
+  };
+
+  const deleteQuery = (key: string) => {
+    setQuery((prev) => {
+      if (!prev) return prev;
+      const { [key]: _, ...rest } = prev;
+      return Object.keys(rest).length ? (rest as S) : undefined;
+    });
+  };
+
+  const formattedQuery = useMemo(() => {
+    if (!query) return "";
+    const searchParams = new URLSearchParams();
+    Object.entries(query).forEach(([key, val]) =>
+      val && searchParams.append(key, String(val))
+    );
+    return `?${searchParams.toString()}`;
+  }, [query]);
 
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
       try {
-        const response = await fetch(url, OPTIONS);
+        const response = await fetch(
+          `${url}${formattedQuery}`,
+          optionsRef.current
+        );
 
         if (!response.ok) {
-          throw new Error(`Error: ${response.statusText}`);
+          const err = await response.json();
+          throw new Error(`Error: ${err.err}`);
         }
 
         const responseData = await response.json();
@@ -60,9 +104,9 @@ const useFetch = <T>(
     };
 
     fetchData();
-  }, [url, OPTIONS]);
+  }, [url, formattedQuery]);
 
-  return { data, isLoading, error };
+  return { data, query, addQuery, deleteQuery, updateQuery, isLoading, error };
 };
 
 export default useFetch;
