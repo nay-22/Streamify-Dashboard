669eee8534076669ce4a440d52bb85c9c48e5181
Nayan <nayan92001@gmail.com>
2025-02-05 01:11:26 +0530

    Added error themes

--- Streamify-frontend/src/constants/ThemeConstants.ts
+++ Streamify-frontend/src/constants/ThemeConstants.ts
@@ -1,31 +1,47 @@
-import { ThemeOptions } from "../types"
+import { ThemeOptions } from "../types";
 
 export const darkTheme: ThemeOptions = {
+  background: {
+    primary: "bg-dark-bg-primary",
+    secondary: "bg-dark-bg-secondary",
+    tertiary: "bg-dark-bg-tertiary",
+    accent: "bg-gradient-to-b from-[#6A0E58] to-[#2A0476]",
+  },
+  text: {
+    primary: "text-dark-text-primary",
+    secondary: "text-dark-text-secondary",
+    tertiary: "text-dark-text-tertiary",
+    accent: "text-dark-text-accent",
+  },
+  error: {
     background: {
-        primary: 'bg-dark-bg-primary',
-        secondary: 'bg-dark-bg-secondary',
-        tertiary: 'bg-dark-bg-tertiary',
-        accent: 'bg-gradient-to-b from-[#6A0E58] to-[#2A0476]'
+      primary: "bg-red-950",
     },
     text: {
-        primary: 'text-dark-text-primary',
-        secondary: 'text-dark-text-secondary',
-        tertiary: 'text-dark-text-tertiary',
-        accent: 'text-dark-text-accent'
-    }
-}
+      primary: "text-gray-400",
+    },
+  },
+};
 
 export const lightTheme: ThemeOptions = {
+  background: {
+    primary: "bg-light-bg-primary",
+    secondary: "bg-light-bg-secondary",
+    tertiary: "bg-light-bg-tertiary",
+    accent: "bg-gradient-to-b from-[#BE9DFF] to-[#FFA3ED]",
+  },
+  text: {
+    primary: "text-light-text-primary",
+    secondary: "text-light-text-secondary",
+    tertiary: "text-light-text-tertiary",
+    accent: "text-light-text-accent",
+  },
+  error: {
     background: {
-        primary: 'bg-light-bg-primary',
-        secondary: 'bg-light-bg-secondary',
-        tertiary: 'bg-light-bg-tertiary',
-        accent: 'bg-gradient-to-b from-[#FFA3ED] to-[#BE9DFF]'
+      primary: "bg-red-300",
     },
     text: {
-        primary: 'text-light-text-primary',
-        secondary: 'text-light-text-secondary',
-        tertiary: 'text-light-text-tertiary',
-        accent: 'text-light-text-accent'
-    }
-}
+      primary: "text-gray-800",
+    },
+  },
+};
