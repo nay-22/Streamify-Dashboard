954cd28312dbb3b7d18a7cd3a9b8732b8edc7740
Nayan <nayan92001@gmail.com>
2025-02-05 01:05:04 +0530

    Persists theme mode in localStorage

--- Streamify-frontend/src/App.tsx
+++ Streamify-frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { useScreenSize } from "./hooks";
 import StreamifyProvider from "./context/providers/StreamifyProvider";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ThemeMode } from "./types";
 import ThemeProvider from "./context/providers/ThemeProvider";
 import { lightTheme, darkTheme } from "./constants/ThemeConstants";
@@ -11,12 +11,19 @@ import "./App.css";
 function App() {
   const { screen, windowSize } = useScreenSize();
 
-  const [themeMode, setThemeMode] = useState<ThemeMode>(ThemeMode.DARK);
+  const [themeMode, setThemeMode] = useState<ThemeMode>(
+    JSON.parse(localStorage.getItem("mode") || "dark") as ThemeMode
+  );
 
+  useEffect(() => {
+    localStorage.setItem("mode", JSON.stringify(themeMode));
+  }, [themeMode]);
 
   return (
     <StreamifyProvider value={{ screen, windowSize, themeMode, setThemeMode }}>
-      <ThemeProvider value={themeMode === ThemeMode.LIGHT ? lightTheme : darkTheme}>
+      <ThemeProvider
+        value={themeMode === ThemeMode.LIGHT ? lightTheme : darkTheme}
+      >
         <Dashboard />
       </ThemeProvider>
     </StreamifyProvider>
